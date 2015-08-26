fdc58676cb6e469e39fade76db047eae4849e53b
Pranav Angara <apranav19@gmail.com>
2015-08-25 22:52:37 -0700

    uber_py_1: Refactor backend and fetch products via angular

--- uber.py
+++ uber.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, session, url_for, request
+from flask import Flask, render_template, redirect, session, url_for, request, jsonify
 from rauth import OAuth2Service
 from user import User
 import requests
@@ -9,30 +9,11 @@ app.secret_key=app.config['APP_SECRET']
 
 @app.route('/')
 def index():
+    current_user = session.get('current_user', None)
     if 'uber_at' in session:
-        return redirect(url_for('profile'))
-    return render_template('index.html')
-
-@app.route('/test_profile')
-def test_profile():
-    return render_template('profile.html')
-
-@app.route('/profile')
-def profile():
-    if 'uber_at' in session:
-        data = requests.get(
-            app.config['API_URL']+'me',
-            headers={
-                'Authorization': 'Bearer {0}'.format(session['uber_at'])
-            }
-        ).json()
-        current_user = User(
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            picture=data['picture']
-        )
-        return render_template('profile.html', user=current_user)
-    return redirect(url_for('index'))
+        if not current_user:
+            current_user = create_user_object()
+    return render_template('index.html', user=current_user)
 
 @app.route('/login')
 def login():
@@ -41,6 +22,45 @@ def login():
     """
     uber_auth_url = create_uber_auth()
     return redirect(uber_auth_url)
+
+@app.route('/logout')
+def logout():
+    """
+        Signs a user out and clears the session data
+    """
+    session.pop('current_user')
+    session.pop('uber_at')
+    return redirect(url_for('index'))
+
+@app.route('/products')
+def get_products():
+    """
+        Fetches the list of products from Uber
+    """
+    if 'uber_at' in session:
+        products_data = requests.get(
+            app.config['API_URL']+'products',
+            headers={
+                'Authorization': 'Bearer {0}'.format(session['uber_at'])
+            },
+            params={
+                'latitude': 37.775818,
+                'longitude': -122.418028,
+            }
+        ).json()
+        return jsonify(products_data)
+    return unauthorized_view("Unauthorized View")
+
+@app.errorhandler(401)
+def unauthorized_view(error=None):
+    message = {
+        'status': 401,
+        'message': 'Error: ' + error,
+    }
+    resp = jsonify(message)
+    resp.status_code = 401
+    return resp
+
 
 @app.route('/callback')
 def login_redirect():
@@ -62,7 +82,6 @@ def login_redirect():
     access_token = response.json().get('access_token')
     if access_token:
         session['uber_at'] = access_token
-        return redirect(url_for('profile'))
     return redirect(url_for('index'))
 
 def create_uber_auth():
@@ -85,6 +104,19 @@ def create_uber_auth():
     }
 
     return uber_obj.get_authorize_url(**uber_params)
+
+def create_user_object():
+    """
+        Creates a new User object and stores basic information in session
+    """
+    user_data = requests.get(
+        app.config['API_URL']+'me',
+        headers={
+            'Authorization': 'Bearer {0}'.format(session['uber_at'])
+        }
+    ).json()
+    session['current_user'] = user_data
+    return user_data
 
 if __name__ == '__main__':
     app.run()
